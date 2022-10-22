ba469b790e09bf8607127bda244a2a10d3594aa1
FluffelPuff <113269946+fluffelpuff@users.noreply.github.com>
2022-10-22 05:35:16 +0200

    Delete test.6.js

--- tests/test.6.js
+++
@@ -1,9 +0,0 @@
-const secp256k1 = require('@vulpemventures/secp256k1-zkp');
-
-
-(async () => {
-    // secp256k1 returns a Promise that must be resolved before using the exported methods
-    const ar = await secp256k1();
-    console.log('Ts')
-    console.log(ar)
-})();
